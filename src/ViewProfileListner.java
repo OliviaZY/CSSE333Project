c8efc53361dad00bd4b1b31978381ef0dac18a64
OliviaZY <zhouy40630@gmail.com>
2018-05-01 21:25:13 -0400

    view profile is working

--- src/ViewProfileListner.java
+++ src/ViewProfileListner.java
@@ -1,15 +1,10 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
+import java.sql.*;
+import java.time.Clock;
 
-import javax.swing.Box;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class ViewProfileListner implements ActionListener {
 	private JFrame frame;
@@ -64,9 +59,11 @@ public class ViewProfileListner implements ActionListener {
 		  l3 = new JLabel("Last Name: ");
 		  l4 = new JLabel("Date of Birth: ");
 		  l5 = new JLabel("State: ");
-		  l6 = new JLabel("College That You Graduated From: ");
+		  l6 = new JLabel("College: ");
 		  l7 = new JLabel("Profession: ");
 		  l8 = new JLabel("Field: ");
+
+
 		  
 		  
 //		  l9 = new JLabel()
@@ -85,22 +82,7 @@ public class ViewProfileListner implements ActionListener {
 		  l7.setFont(new Font("Serif", Font.BOLD, 20));
 		  l8.setForeground(Color.blue);
 		  l8.setFont(new Font("Serif", Font.BOLD, 20));
-		  
-//		  l12.setForeground(Color.BLACK);
-//		  l12.setFont(new Font("Arial", Font.BOLD, 16));
-//		  l13.setForeground(Color.BLACK);
-//		  l13.setFont(new Font("Arial", Font.BOLD, 16));
-//		  l14.setForeground(Color.BLACK);
-//		  l14.setFont(new Font("Arial", Font.BOLD, 16));
-//		  l15.setForeground(Color.BLACK);
-//		  l15.setFont(new Font("Arial", Font.BOLD, 16));
-//		  l9.setForeground(Color.BLACK);
-//		  l9.setFont(new Font("Arial", Font.BOLD, 16));
-//		  l10.setForeground(Color.BLACK);
-//		  l10.setFont(new Font("Arial", Font.BOLD, 16));
-//		  l11.setForeground(Color.BLACK);
-//		  l11.setFont(new Font("Arial", Font.BOLD, 16));
-		  		 
+
 		  l2.setBounds(80, 70, 200, 30);
 		  l3.setBounds(80, 110, 200, 30);
 		  l4.setBounds(80, 150, 200, 30);
@@ -116,8 +98,113 @@ public class ViewProfileListner implements ActionListener {
 		  frame1.add(l6);
 		  frame1.add(l7);
 		  frame1.add(l8);
-		
-		
+
+		try {
+			CallableStatement cs = this.dbc.prepareCall("call viewProfile(?,?,?,?,?,?,?,?,?)");
+			cs.setString(1, uname);
+			cs.registerOutParameter(2, java.sql.Types.DATE);
+			cs.registerOutParameter(3, java.sql.Types.VARCHAR);
+			cs.registerOutParameter(4, java.sql.Types.VARCHAR);
+			cs.registerOutParameter(5, java.sql.Types.VARCHAR);
+			cs.registerOutParameter(6, java.sql.Types.VARCHAR);
+			cs.registerOutParameter(7, java.sql.Types.VARCHAR);
+			cs.registerOutParameter(8, java.sql.Types.VARCHAR);
+			cs.registerOutParameter(9, java.sql.Types.INTEGER);
+			System.out.println(cs);
+			cs.execute();
+			int ret = cs.getInt(9);
+			if (ret == 1) {
+				JOptionPane.showMessageDialog((Component)null, "ERROR: user name cannot be empty");
+			} else if (ret == 2) {
+				JOptionPane.showMessageDialog((Component)null, "ERROR: incorrect username.");
+			}  else {
+
+
+				if (cs.getString(3)!=null){
+					l10 = new JLabel(cs.getString(3));
+				}else{
+					l10 = new JLabel("       ");
+				}
+
+				if (cs.getString(4)!=null) {
+					l11 = new JLabel(cs.getString(4));
+				}else{
+					l11 = new JLabel("       ");
+				}
+
+				if (cs.getString(5)!=null) {
+					l12 = new JLabel(cs.getString(5));
+				}else{
+					l12 = new JLabel("       ");
+				}
+				if (cs.getString(6)!=null) {
+					l13 = new JLabel(cs.getString(6));
+				}else{
+					l13 = new JLabel("       ");
+				}
+				if (cs.getString(7)!=null) {
+					l14 = new JLabel(cs.getString(7));
+				}else{
+					l14 = new JLabel("       ");
+				}
+				if (cs.getString(8)!=null) {
+					l15 = new JLabel(cs.getString(8));
+				}else{
+					l15 = new JLabel("       ");
+				}
+
+				if (cs.getDate(2)!=null) {
+					l9 = new JLabel(cs.getDate(2).toString());
+				}else{
+					l9 = new JLabel("       ");
+				}
+//				l12 = new JLabel(cs.getString(5));
+//				l13 = new JLabel();
+//				l14 = new JLabel();
+//				l15 = new JLabel();
+//				System.out.println(l9.getText());
+				l12.setForeground(Color.BLACK);
+			    l12.setFont(new Font("Arial", Font.BOLD, 16));
+			    l13.setForeground(Color.BLACK);
+			    l13.setFont(new Font("Arial", Font.BOLD, 16));
+			    l14.setForeground(Color.BLACK);
+			    l14.setFont(new Font("Arial", Font.BOLD, 16));
+			    l15.setForeground(Color.BLACK);
+			    l15.setFont(new Font("Arial", Font.BOLD, 16));
+			    l9.setForeground(Color.BLACK);
+			    l9.setFont(new Font("Arial", Font.BOLD, 16));
+			    l10.setForeground(Color.BLACK);
+			    l10.setFont(new Font("Arial", Font.BOLD, 16));
+			    l11.setForeground(Color.BLACK);
+			    l11.setFont(new Font("Arial", Font.BOLD, 16));
+
+
+				l9.setBounds(300, 70, 200, 30);
+				l10.setBounds(300, 110, 200, 30);
+				l11.setBounds(300, 150, 200, 30);
+				l12.setBounds(300, 190, 200, 30);
+				l13.setBounds(300, 230, 200, 30);
+				l14.setBounds(300, 270, 200, 30);
+				l15.setBounds(300, 310, 200, 30);
+
+				frame1.add(l9);
+				frame1.add(l10);
+				frame1.add(l11);
+				frame1.add(l12);
+				frame1.add(l13);
+				frame1.add(l14);
+				frame1.add(l15);
+//				frame1.add(l16);
+
+			}
+
+		}catch (SQLException var13){
+			var13.printStackTrace();
+			JOptionPane.showMessageDialog((Component)null, "Login Failed");
+		}
+
+
+
 //		links.add(closeConnection);
 //		for (JButton j:buttonLinks){
 //			j.addActionListener(new LinksListener(j.getText(),frame1,dbc));
