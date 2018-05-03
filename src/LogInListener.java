b9d5bdff98188302eae8dd96aff7eb327565ff5a
OliviaZY <zhouy40630@gmail.com>
2018-05-03 10:15:54 -0400

    search for a friend panel

--- src/LogInListener.java
+++ src/LogInListener.java
@@ -77,13 +77,14 @@ public class LogInListener implements ActionListener {
 
 					//Link buttons on the left side of the screen
 					Box links = Box.createVerticalBox();
-					JButton[] buttonLinks = new JButton[5];
+					JButton[] buttonLinks = new JButton[6];
 					buttonLinks[0] = new JButton("Profile");
 					buttonLinks[0].addActionListener(new ViewProfileListner(tf1.getText(),frame1,dbc));
 					buttonLinks[1] = new JButton("Friends");//TODO: add code in action listener for this
 					buttonLinks[2] = new JButton("Reminders");//TODO: add code in action listener for this
 					buttonLinks[3] = new JButton("Events");
 					buttonLinks[4] = new JButton("Interests");
+					buttonLinks[5] = new JButton("search for a friend");
 					JButton closeConnection = new JButton("Close Connection");//THis should probably be changed to something automatic
 //					closeConnection.addActionListener(new ConnectionCloser(dbc));//but it's 3AM so that's beyond my abilites rn
 					links.add(closeConnection);
