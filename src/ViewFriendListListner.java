4fc610751ffd5f3adfe2ab3bc796fbfb572c559d
OliviaZY <zhouy40630@gmail.com>
2018-05-15 22:45:55 -0400

    add missing file

--- src/ViewFriendListListner.java
+++ src/ViewFriendListListner.java
@@ -32,84 +32,56 @@ public class ViewFriendListListner implements ActionListener {
 		frame1.setSize(1000, 1000);
 
 		try {
-			CallableStatement cs = this.dbc.prepareCall("call listOfFriendInit(?,?)");
+			// check if the user name is contained in the usernameinit attribute
+			CallableStatement cs = this.dbc.prepareCall("call ListOfFriendInit(?,?)");
 			cs.setString(1, username.getText());
 			cs.registerOutParameter(2, java.sql.Types.INTEGER);
-
 			cs.execute();
 			int ret = cs.getInt(2);
-			// JButton[] buttonLinksAcc = new JButton[ret];
-			// JButton[] buttonLinksDec = new JButton[ret];
+			ResultSet rs = cs.executeQuery();
+			
+			// check if the user name is contained in the
+			// usernameseached attribute
+			CallableStatement cs2 = this.dbc.prepareCall("call listOfFriendSearched(?,?)");
+			cs2.setString(1, username.getText());
+			cs2.registerOutParameter(2, java.sql.Types.INTEGER);
+			cs2.execute();
+			int ret2 = cs2.getInt(2);
+			ResultSet rs2 = cs2.executeQuery();
+
 			if (ret == 1) {
 				JOptionPane.showMessageDialog((Component) null, "ERROR: user name cannot be empty");
-			} else if (ret == 2) {
+			} else if (ret == 2 && ret2 == 2) {
 				JOptionPane.showMessageDialog((Component) null, "Dude, get a friend!");
 			} else {
-				System.out.println(cs);
-				ResultSet rs = cs.executeQuery();
-				System.out.println(rs);
-
-				// for (int i =0 ; i<ret;i++){
-				// buttonLinksAcc[i]=new JButton("accpet as a friend");
-				// buttonLinksAcc[i].setForeground(Color.blue);
-				// buttonLinksAcc[i].setFont(new Font("Serif", Font.BOLD, 20));
-				// buttonLinksAcc[i].setBounds(600, 400+30*i, 100, 30);
-				// frame1.add(buttonLinksAcc[i]);
-				//
-				//
-				// buttonLinksDec[i]=new JButton("decline");
-				// buttonLinksDec[i].setForeground(Color.blue);
-				// buttonLinksDec[i].setFont(new Font("Serif", Font.BOLD, 20));
-				// buttonLinksDec[i].setBounds(750, 400+30*i, 100, 30);
-				// frame1.add(buttonLinksDec[i]);
-				//
-				// }
 				int i = 0;
-				// while (rs.next()){
-				// i++;
-				// }
-				// System.out.println("this is a test for counting the total row
-				// of
-				// the result set");
-				// System.out.println(i);
 				while (rs.next()) {
-
 					System.out.println(i);
 					JLabel l1 = new JLabel(rs.getString(1));
-					// buttonLinksDec[i].addActionListener(new
-					// declineFriendRequestListner(username.getText(),l1.getText(),
-					// frame1,dbc));
-					// buttonLinksAcc[i].addActionListener(new
-					// acceptFriendRequestListner(username.getText(),l1.getText(),
-					// frame1,dbc));
-
-					// JButton accept = new JButton("accept as a friend!");
-					// rs.next();
 					System.out.println("this is a test for select the first row in the result set");
-					l1.setBounds(100, 300 * i, 200, 30);
-					// accept.setPreferredSize(new Dimension(100,50));
+					l1.setBounds(100, 200 * i, 200, 30);
 					l1.setForeground(Color.blue);
 					l1.setFont(new Font("Serif", Font.BOLD, 40));
-					// accept.setForeground(Color.blue);
-					// accept.setFont(new Font("Serif", Font.BOLD, 40));
-					// l1.setBounds(80, 70, 200, 30);
-					// frame1.add(accept);
 					frame1.add(l1);
 					i++;
-
 				}
-				frame1.setVisible(true);
-				// for (int i = 1; i<rs.getRow();i++){
-				// JLabel l1 = new JLabel(rs.getString(i));
-				// System.out.println(l1.getText());
-				// System.out.println("this is a test for select the first row
-				// in
-				// the result set");
-				// l1.setBounds(100*i, 100*i, 200, 30);
-				// frame1.add(l1);
-				// frame1.setVisible(true);
-				// }
+				if (ret2 == 3) {
+					while (rs2.next()) {
+						System.out.println(i);
+						JLabel l1 = new JLabel(rs2.getString(1));
+						System.out.println("this is a test for select the first row in the result set");
+						l1.setBounds(100, 200 * i, 200, 30);
+						l1.setForeground(Color.blue);
+						l1.setFont(new Font("Serif", Font.BOLD, 40));
+						frame1.add(l1);
+						i++;
+					}
+				}
+
+
 			}
+			frame1.setVisible(true);
+
 		} catch (SQLException var13) {
 			var13.printStackTrace();
 			JOptionPane.showMessageDialog((Component) null, "view friend failed");
